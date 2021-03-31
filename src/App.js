8f2e97eb1e77a9a5247a4818b9023d0726c4aa34
Gioele Farina <jchilosa@gmail.com>
2021-03-31 15:39:27 +0200

    victory handler part1

--- src/App.js
+++ src/App.js
@@ -15,9 +15,14 @@ class App extends Component {
     isP2Uman: true,
 
     isP1turn: true,
+    isGameOver: false,
   }
 
   moveHandler = (cellIndex) => {
+    if (this.state.isGameOver) {
+      return null;
+    }
+
     let move = this.state.isP1turn ? "X" : "O";
     let newCells = [...this.state.cells];
 
@@ -28,6 +33,47 @@ class App extends Component {
       cells: newCells,
       isP1turn: !(this.state.isP1turn),
     });
+
+    this.victoryHandler(newCells);
+  }
+
+  victoryHandler= (cells) => {
+    let board = cells;
+
+    let victoryto = () => {
+      this.state.isGameOver = true;
+      if (this.state.isP1turn) {
+        console.log("P1 wins");
+      } else {
+        console.log("P2 wins");
+      }
+    }
+
+    if ((board[0] === "X" && board[1] === "X" && board[2] === "X") ||
+        (board[0] === "O" && board[1] === "O" && board[2] === "O")) {
+          victoryto();
+    } else if ((board[3] === "X" && board[4] === "X" && board[5] === "X") ||
+              (board[3] === "O" && board[4] === "O" && board[5] === "O")) {
+          victoryto();
+    } else if ((board[6] === "X" && board[7] === "X" && board[8] === "X") ||
+              (board[6] === "O" && board[7] === "O" && board[8] === "O")) {
+          victoryto();
+    } else if ((board[6] === "X" && board[4] === "X" && board[2] === "X") ||
+              (board[6] === "O" && board[4] === "O" && board[2] === "O")) {
+          victoryto();
+    } else if ((board[0] === "X" && board[3] === "X" && board[6] === "X") ||
+              (board[0] === "O" && board[3] === "O" && board[6] === "O")) {
+          victoryto();
+    } else if ((board[1] === "X" && board[4] === "X" && board[7] === "X") ||
+              (board[1] === "O" && board[4] === "O" && board[7] === "O")) {
+          victoryto();
+    } else if ((board[2] === "X" && board[5] === "X" && board[8] === "X") ||
+              (board[2] === "O" && board[5] === "O" && board[8] === "O")) {
+          victoryto();
+    } else if ((board[0] === "X" && board[4] === "X" && board[8] === "X") ||
+              (board[0] === "O" && board[4] === "O" && board[8] === "O")) {
+          victoryto();
+    }
   }
 
   componentDidMount() {
